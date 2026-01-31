8e8647b09647d9077c072e499af863242f263831
xorwns56 <49503911+xorwns56@users.noreply.github.com>
2026-02-01 02:06:52 +0900

    chore(TASK-11) : 백엔드 서비스 생성 (#3)

---
+++ backend/fitting-service/src/test/java/com/xorwns56/fitting/FittingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xorwns56.fitting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
